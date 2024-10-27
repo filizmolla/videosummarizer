534a308a0898b21a91727b7b263be333ecef1c62
Filiz Molla <filiz.molla01@gmail.com>
2024-10-28 02:10:46 +0300

    removed unnecessary code.

--- extract-pdf-text.py
+++ extract-pdf-text.py
@@ -1,10 +1,9 @@
-import pymupdf
-print("bilmemne")
-doc = pymupdf.open("reactbook.pdf") 
+import fitz
+
+doc = fitz.open("reactbook.pdf") 
 out = open("output-book.txt", "wb") # create a text output
 for page in doc: # iterate the document pages
     text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
     out.write(text) # write text of page
     out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
 out.close()
-print("hello world ")
